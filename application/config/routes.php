d935f004b10c599eceede9be973739577e3462a6
mangedod <aqouzzz@gmail.com>
2024-01-30 10:36:03 +0700

    selasa, 30 Jan 2024

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,4 +4,5 @@ $route['default_controller'] = 'id';
 $route['404_override'] = '';
 $route['dashboard'] = 'is';
 $route['profile'] = 'is/editprofil';
+$route['settings'] = 'is/settings';
 $route['translate_uri_dashes'] = false;
